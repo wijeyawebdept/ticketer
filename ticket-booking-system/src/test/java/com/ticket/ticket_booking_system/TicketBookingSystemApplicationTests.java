65f1729e12bfefe1a197b3cd88a0f1a0ed38678a
YourName <havindunimadith7@gmail.com>
2025-09-29 12:02:49 +0530

    project initialized

---
+++ ticket-booking-system/src/test/java/com/ticket/ticket_booking_system/TicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.ticket_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
